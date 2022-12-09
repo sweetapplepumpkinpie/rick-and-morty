83f9c78d689b04ee76d5169956e064d7f2c4819d
sweetapplepumpkinpie <sweetapplepumpkinpie@gmail.com>
2022-12-09 03:06:38 -0500

    fix the testing problem

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders characters", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const titleElement = screen.getByText(/characters/i);
   expect(titleElement).toBeInTheDocument();
 });
